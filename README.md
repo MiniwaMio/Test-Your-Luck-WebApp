e7b57fbc4f624d6d5eb1c6407279bb933d8c05d2
MiniwaMio <66834138+MiniwaMio@users.noreply.github.com>
2025-03-20 09:35:28 +0800

    Quick update 1
    -Added bcrpyt for password encrpytion

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Test Your Luck WebApp
- 
+# Test Your Luck WebApp by MTech
+
+## 1. Introduction
+
+Hi, This is Miniwa from MTech.  
+This is one of the many projects that I will be making to learn the Postgres, Express, React, Node (PERN Stack).
+
+## 2. Game Mechanics
+
+Test Your Luck is a simple web game that assigns a 4 digit hidden number every day and each player will get 5 attempts per day to guess the correct number.  
+If you successfully guessed a number. CONGRATULATIONS!! You are lucky!! You get a point!!  
+If you didn't, try again tomorrow.
+
+There will be leaderboard so you can boast to your friends that you are indeed lucky. :>
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
+const bcrypt = require("bcrypt");
 
 //middleware
 app.use(cors());
@@ -10,7 +11,46 @@ app.use(express.json());
 
 //Routes
 
-//Adding an attempt
+//Register an account
+app.post("/register", async (req, res) => {
+  try {
+    //{needs to match parameter}
+    const { username } = req.body;
+    const { password } = req.body;
+    console.log(username, password);
+    //encrypting password with bcrypt
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const result = await pool.query(
+      "INSERT INTO users (username, user_pass, score) VALUES($1,$2,0) RETURNING *",
+      [username, hashedPassword]
+    );
+    console.log(result);
+    res.json({
+      message: "User registered successfully",
+      user: result.rows[0],
+    });
+  } catch (err) {
+    console.log("Error at registration");
+  }
+});
+
+//Authenticate
+app.post("/Login", async (req, res) => {
+  try {
+    const { uname } = req.body.username;
+    const { upass } = req.body.password;
+    const checkUser = await pool.query(
+      "SELECT * FROM users WHERE username = $uname AND user_pass = $upass",
+      [uname, upass]
+    );
+
+    res.json(newUser);
+  } catch (err) {
+    console.log("Error at Login");
+  }
+});
 
 app.listen(5000, () => {
   console.log("Server has started on port 5000");
