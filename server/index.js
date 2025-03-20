8a350a0851c35e77de47117029326bea34a22b5f
MiniwaMio <66834138+MiniwaMio@users.noreply.github.com>
2025-03-21 01:52:35 +0800

    updated index.js
    -Completed Login function

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.post("/register", async (req, res) => {
     //{needs to match parameter}
     const { username } = req.body;
     const { password } = req.body;
-    console.log(username, password);
+
     //encrypting password with bcrypt
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
@@ -26,7 +26,7 @@ app.post("/register", async (req, res) => {
       "INSERT INTO users (username, user_pass, score) VALUES($1,$2,0) RETURNING *",
       [username, hashedPassword]
     );
-    console.log(result);
+
     res.json({
       message: "User registered successfully",
       user: result.rows[0],
@@ -37,20 +37,40 @@ app.post("/register", async (req, res) => {
 });
 
 //Authenticate
-app.post("/Login", async (req, res) => {
+app.post("/login", async (req, res) => {
   try {
-    const { uname } = req.body.username;
-    const { upass } = req.body.password;
-    const checkUser = await pool.query(
-      "SELECT * FROM users WHERE username = $uname AND user_pass = $upass",
-      [uname, upass]
-    );
+    const { username } = req.body;
+    const { password } = req.body;
 
-    res.json(newUser);
+    const result = await pool.query("SELECT * FROM users WHERE username = $1", [
+      username,
+    ]);
+
+    if (result.rows.length === 0) {
+      return res.status(401).json({ error: "invalid username or password" });
+    }
+
+    const user = result.rows[0];
+
+    const isValid = await bcrypt.compare(password, user.user_pass);
+
+    if (!isValid) {
+      return res.status(401).json({ error: "invalid username or password" });
+    }
+
+    res.status(500).json({
+      message: "Login successful",
+      id: user.id,
+      username: user.username,
+    });
   } catch (err) {
     console.log("Error at Login");
   }
 });
+
+//Update Score
+
+
 
 app.listen(5000, () => {
   console.log("Server has started on port 5000");
